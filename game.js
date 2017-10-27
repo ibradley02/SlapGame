71749410177efdcd05aed5cb061b9891a9f60188
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-27 13:20:47 -0600

    can now give items w/ constructor

--- game.js
+++ game.js
@@ -1,52 +1,59 @@
-var bigBoss = new Target(100, 'King K. Rool', 0, 0)
+var bigBoss = new Target(100, 'King K. Rool', 0)
+var banana = new Item('banana', 1, 'Yellow and Tasty? Must be the banana')
+var tnt = new Item('TNT', 1, 'Cause I\'m T.N.T. I\'m dynamite')
+var barrel = new Item('BARREL', 1, 'It\'s a barrel... and a missile?')
 var hits = 0
 var health = 100
-function Target(health, name, hits, items){
+function Target(health, name, hits, items) {
     this.health = health
     this.name = name
     this.hits = hits
     this.items = []
 }
-function Items(name, modifier, description){
+function Item(name, modifier, description) {
     this.name = name
     this.modifier = modifier
     this.description = description
 }
-var items = {
-    banana: new Items('banana', 0.3, 'Yellow and Tasty? Must be the banana'),
-    TNT: new Items('TNT', 0.3, 'Cause I\'m T.N.T. I\'m dynamite'),
-    BARREL: new Items('BARREL', 0.4, 'It\'s a barrel... and a missile?')
-}
-function slap(){
+function slap() {
     hits += 1;
     health -= 1;
     update()
     return
 }
-function kick(){
+function kick() {
     health -= 10;
-    hits += 1;    
+    hits += 1;
     update()
     return
 }
-function punch(){
+function punch() {
     health -= 5;
-    hits += 1;    
+    hits += 1;
     update()
     return
 }
-function update(){
+function update() {
     document.getElementById('health').innerText = health
     document.getElementById('hits').innerText = hits
     document.getElementById('name').innerText = name
 }
-update()
-function giveBanana(){
-    bigBoss.items.push(items.banana);
+function giveBanana(banana) {
+    bigBoss.items.push(banana)
 }
-function giveTNT(){
-    bigBoss.items.push(items.TNT);
+function giveItem(item) {
+    bigBoss.items.push(item)
 }
-function giveBARREL(){
-    bigBoss.items.push(items.BARREL);
-}
+var totalMods = 0
+// function addMods(){
+//     for (var i = 0; i < bigBoss.items.length; i++) {
+//         var modValue = bigBoss.items.modifier[i];
+//         totalMods += modValue
+//     }
+//     return totalMods
+// }
+update();
+giveItem(banana);
+giveItem(tnt);
+giveItem(barrel);
+console.log(bigBoss)
