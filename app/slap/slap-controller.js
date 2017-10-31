88eacbb2f26a6d386f74f38efdc1f009f2f3503e
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-31 14:07:43 -0600

    CHANGES TO UPDATE FUNCTION

--- app/slap/slap-controller.js
+++ app/slap/slap-controller.js
@@ -1,11 +1,15 @@
 function SlapController(){
     var slapService = new SlapService()
     //PRIVATE PARTS
-    function update(target) {
-        document.getElementById('health').innerText = bigBoss.health
-        document.getElementById('hits').innerText = bigBoss.hits
-        document.getElementById('name').innerText = bigBoss.name
+    function update() {
+        var target = slapService.getTarget()
+        document.getElementById('health').innerText = target.health
+        document.getElementById('hits').innerText = target.hits
+        document.getElementById('name').innerText = target.name
         document.getElementById('text-display').innerText = ''
+        if (target.health <= 0){
+            document.getElementById('text-display').innerText = 'YOU HAVE DEFEATED KING K. RULE! YOU DESERVE A BANANA!'
+        }
     }
     //PUBLIC PARTS
     update()
