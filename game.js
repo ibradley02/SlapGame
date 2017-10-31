9d5780458c6344be7e07de7757e809491458924b
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-31 12:46:10 -0600

    deleted returns

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var bigBoss = new Target(100, 'King K. Rool', 0)
 var banana = new Item('banana', 1, 'Yellow and Tasty? Must be the banana')
 var tnt = new Item('TNT', 10, 'Cause I\'m T.N.T. I\'m dynamite')
 var barrel = new Item('BARREL', 0.5, 'It\'s a barrel... and a missile?')
-var totalMods = 0
+totalMods = 0
 update(bigBoss);
 function Target(health, name, hits, items) {
     this.health = health
@@ -19,19 +19,16 @@ function slap() {
     bigBoss.hits += 1;
     imDead(bigBoss.health -= (1 + totalMods));
     update(bigBoss)
-    return
 }
 function kick() {
     bigBoss.hits += 1;
     imDead(bigBoss.health -= (10 + totalMods))
     update(bigBoss)
-    return
 }
 function punch() {
     bigBoss.hits += 1;
     imDead(bigBoss.health -= (5 + totalMods));
     update(bigBoss)
-    return
 }
 function update(target) {
     document.getElementById('health').innerText = target.health
@@ -51,7 +48,7 @@ function addMods() {
     }
     return totalMods
 }
-function imDead() {
+function imDead(target) {
     if (bigBoss.health <= 0) {
         bigBoss.health = 0;
         alert('YOU HAVE DEFEATED KING K. RULE')
