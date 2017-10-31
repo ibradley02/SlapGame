e651eb20a9012697ba8f93e2fc36f524197ebf87
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-31 15:00:07 -0600

    added debugger

--- app/slap/slap-service.js
+++ app/slap/slap-service.js
@@ -47,6 +47,7 @@ function SlapService(){
         return (JSON.parse(JSON.stringify(items)))
     }
     this.reset = function reset(){
+        debugger
        var bigBoss = new Target(100, 'King K. Rool', 0);
         totalMods = 0;
     }
