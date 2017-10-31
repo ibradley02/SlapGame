4f04f34f9a220d582e4fd3ed157fd54508979a6e
Ian Bradley <ianbradleydebate@gmail.com>
2017-10-31 14:03:08 -0600

    chchhcanges

--- app/slap/slap-controller.js
+++ app/slap/slap-controller.js
@@ -2,9 +2,9 @@ function SlapController(){
     var slapService = new SlapService()
     //PRIVATE PARTS
     function update(target) {
-        document.getElementById('health').innerText = target.health
-        document.getElementById('hits').innerText = target.hits
-        document.getElementById('name').innerText = target.name
+        document.getElementById('health').innerText = bigBoss.health
+        document.getElementById('hits').innerText = bigBoss.hits
+        document.getElementById('name').innerText = bigBoss.name
         document.getElementById('text-display').innerText = ''
     }
     //PUBLIC PARTS
